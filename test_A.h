c3f18d6c104943bae87576cf60550b1bac9d8d56
LitvinovaE <litvinova.eugeniya@yandex.ru>
2018-06-18 23:37:24 +0300

    Added .h files

---
+++ test_A.h
@@ -0,0 +1,8 @@
+#ifndef TEST_A
+#define TEST_A
+
+#include <stdio.h>
+
+void A();
+
+#endif	// TEST_A
---
+++ test_B.h
@@ -0,0 +1,10 @@
+#ifndef TEST_B
+#define TEST_B
+
+#include <stdio.h>
+#include "test_A.h"
+
+void B();
+void C();
+
+#endif // TEST_B
